9fb24a0d92db54b268840cedea6b365f245ecbba
Liamdoult <3579535@myuwc.ac.za>
2025-01-18 07:17:52 +0100

    Implement RFC7519 Section 5 JOSE Header (only title section)
    Adds tests for Section 5 that describes the basic concept of JOSE Header.
    
    Majority of the content is in reference to RFC JWS (7515) & JWE (7516) which
    are out of scope. Thus majority of this can be excluded as meta.

---
+++ test/jwt.RFC7519.Test/Section5.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using jwt.Options;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace jwt.RFC7519.Test;
+
+/// <summary>
+/// Asserts Section 5 JOSE Header
+///
+/// For a JWT object, the members of the JSON object represented by the JOSE
+/// Header describe the cryptographic operations applied to the JWT and
+/// optionally, additional properties of the JWT. Depending upon whether the JWT
+/// is a JWS or JWE, the corresponding rules for the JOSE Header values apply.
+/// This specification further specifies the use of the following Header
+/// Parameters in both the cases where the JWT is a JWS and where it is a JWE.
+/// </summary>
+[TestClass]
+public class Section5 {
+
+    /// <summary>
+    /// Validates token with optional additional properties.
+    /// </summary>
+    [TestMethod]
+    public void WhenOptionalClaim_ThenSucceeds() {
+        // Token with custom header value "custom": "custom"
+        const string raw = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImN1c3RvbSI6ImN1c3RvbSJ9.eyJqdGkiOiJpZGVudGlmaWVyIn0.lfx5tkSsJGu2WirOcJczR5_-KP9wRyH8YIOPuq0sE3M";
+
+        new JwtHandler(TestDefaults.DefaultTestOptions).TryGetValue(raw, out var token, out var error).Should().BeTrue();
+    }
+}
